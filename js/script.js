cf8102080aa2e28c7ea91be58d185fe9355d5270
Christopher Sy <christophersy@Christophers-Laptop.local>
2024-05-02 22:54:58 -0700

    Commit 2 - Add boilerplate and link files. JS arrays and objects for game payouts. Function blue print for win conditions.

---
+++ js/script.js
@@ -0,0 +1,217 @@
+// alert("Am I working?")
+
+//Roulette Game
+
+//Payouts
+//SINGLE NUMBER PAYS 35 to 1 ((35 x BET) + Original Bet = Winnings)
+//#0,00,1-36 (38 total numbers) 
+
+// function () {
+//     if (winConditionSingle) {
+//     return ((`Total Winnings: (${userBet} x 2) + ${userBet})`))
+//     }else {
+//         return ((`Round Lost. ${userWallet} - ${userBet}`))
+//     }
+// }
+
+// let wheelNum = ["0","00",1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36]
+
+
+//DOUBLE NUMBER PAYS 17 to 1 ((17 x BET) + Original Bet = Winnings)
+
+// function () {
+//     if (winConditionDouble) {
+//     return ((`Total Winnings: (${userBet} x 17) + ${userBet})`))
+//     }else {
+//         return ((`Round Lost. ${userWallet} - ${userBet}`))
+//     }
+// }
+
+// let twoNum = {
+//     twoA: [1,2]
+//     twoB: [1,4]
+//     twoC: [2,3]
+//     twoD: [2,5]
+//     twoE: [3,6]
+//     twoF: [4,5]
+//     twoG: [4,7]
+//     twoH: [5,6]
+//     twoI: [5,8]
+//     twoJ: [6,9]
+//     twoK: [7,8]
+//     twoL: [7,10]
+//     twoM: [8,9]
+//     twoN: [8,11]
+//     twoO: [9,12]
+//     twoP: [10,11]
+//     twoQ: [10,13]
+//     twoR: [11,12]
+//     twoS: [11,14]
+//     twoT: [12,15]
+//     twoU: [13,14]
+//     twoV: [13,16]
+//     twoW: [14,15]
+//     twoX: [14,17]
+//     twoY: [15,18]
+//     twoZ: [16,17]
+//     twoAa: [16,19]
+//     twoAb: [17,18]
+//     twoAc: [17,20]
+//     twoAd: [18,21]
+//     twoAe: [19,20]
+//     twoAf: [19,22]
+//     twoAg: [20,21]
+//     twoAh: [20,23]
+//     twoAi: [21,24]
+//     twoAj: [22,23]
+//     twoAk: [22,25]
+//     twoAl: [23,24]
+//     twoAm: [23,26]
+//     twoAn: [24,27]
+//     twoAo: [25,26]
+//     twoAp: [25,28]
+//     twoAq: [26,27]
+//     twoAr: [26,29]
+//     twoAs: [27,30]
+//     twoAt: [28,29]
+//     twoAu: [28,31]
+//     twoAv: [29,30]
+//     twoAw: [29,32]
+//     twoAx: [30,33]
+//     twoAy: [31,32]
+//     twoAz: [31,34]
+//     twoBa: [32,33]
+//     twoBb: [32,35]
+//     twoBc: [33,36]
+//     twoBd: [34,35]
+//     twoBe: [35,36]
+// }
+
+
+//THREE NUMBER PAYS 11 to 1 ((11 x BET) + Original Bet = Winnings)
+
+// function () {
+//     if (winConditionThree) {
+//     return ((`Total Winnings: (${userBet} x 11) + ${userBet})`))
+//     }else {
+//         return ((`Round Lost. ${userWallet} - ${userBet}`))
+//     }
+// }
+
+// let threeNum = {
+//     threeA: [1,2,3],
+//     threeB: [4,5,6],
+//     threeC: [7,8,9],
+//     threeD: [10,11,12],
+//     threeE: [13,14,15],
+//     threeF: [16,17,18],
+//     threeG: [19,20,21],
+//     threeH: [22,23,24],
+//     threeI: [25,26,27],
+//     threeJ: [28,29,30],
+//     threeK: [31,32,33],
+//     threeL: [34,35,36]
+// }
+
+//FOUR NUMBER BET PAYS 8 to 1 ((8 x BET) + Original Bet = Winnings)
+
+// function () {
+//     if (winConditionFour) {
+//     return ((`Total Winnings: (${userBet} x 8) + ${userBet})`))
+//     }else {
+//         return ((`Round Lost. ${userWallet} - ${userBet}`))
+//     }
+// }
+
+// let fourNum = {
+//     fourA: [1,2,3,4],
+//     fourB: [2,3,5,6],
+//     fourC: [4,5,7,8],
+//     fourD: [5,6,8,9],
+//     fourE: [7,8,10,11],
+//     fourF: [8,9,11,12],
+//     fourG: [10,11,13,14],
+//     fourH: [11,12,14,15],
+//     fourI: [13,14,16,17],
+//     fourJ: [14,15,17,18],
+//     fourK: [16,17,19,20],
+//     fourL: [17,18,20,21],
+//     fourM: [19,20,22,23],
+//     fourN: [20,21,23,24],
+//     fourO: [22,23,25,26],
+//     fourP: [23,24,26,27],
+//     fourQ: [25,26,28,29],
+//     fourR: [26,27,29,30],
+//     fourS: [28,29,31,32],
+//     fourT: [29,30,32,33],
+//     fourU: [31,32,34,35],
+//     fourV: [32,33,35,36],
+// }
+
+
+//FIVE NUMBER BET PAYS 6 to 1 ((6 x BET) + Original Bet = Winnings)
+
+// function () {
+//     if (winConditionFive) {
+//     return ((`Total Winnings: (${userBet} x 6) + ${userBet})`))
+//     }else {
+//         return ((`Round Lost. ${userWallet} - ${userBet}`))
+//     }
+// }
+
+// let fiveNum = ["0","00",1,2,3]
+
+
+//SIX NUMBER BET PAYS 5 to 1 ((5 x BET) + Original Bet = Winnings)
+
+// function () {
+//     if (winConditionSix) {
+//     return ((`Total Winnings: (${userBet} x 5) + ${userBet})`))
+//     }else {
+//         return ((`Round Lost. ${userWallet} - ${userBet}`))
+//     }
+// }
+
+// let sixNum = {
+//     sixA: [1,2,3,4,5,6],
+//     sixB: [7,8,9,10,11,12],
+//     sixC: [13,14,15,16,17,18],
+//     sixD: [19,20,21,22,23,24],
+//     sixE: [25,26,27,28,29,30],
+//     sixF: [31,32,33,34,35,36]
+// }
+
+
+//DOZENS AND COLUMNS PAY 2 to 1 ((2 x BET) + Original Bet = Winnings)
+
+// function () {
+//     if (winConditionDozen) {
+//     return ((`Total Winnings: (${userBet} x 2) + ${userBet})`))
+//     }else {
+//         return ((`Round Lost. ${userWallet} - ${userBet}`))
+//     }
+// }
+
+//Dozen 1 (1-12) .....let dozenOne = [1,2,3,4,5,6,7,8,9,10,11,12] (let i=1; i<=12; i++);
+//Dozen 2 (13-24) .....let dozenTwo = [12,13,14,15,16,17,18,19,20,21,22,23,24] (let i=12; i<=24; i++);
+//Dozen 3 (25-36) .....let dozenThree = [25,26,27,28,29,30,31,32,33,34,35,36] (let i=25; i<=36; i++)
+//Row 1 (1,4,7,10,13,16,19,22,25,28,31,34) ....let rowOne = [1,4,7,10,13,16,19,22,25,28,31,34] (let i = 1; i<=34; i+3);
+//Row 2 (2,5,8,11,14,17,20,23,26,29,32,35) ....let rowTwo = [2,5,8,11,14,17,20,23,26,29,32,35] (let i = 2; i<=35; i+3);
+//Row 3 (3,6,9,12,15,18,21,24,27,30,33,36) ....let rowThree = [3,6,9,12,15,18,21,24,27,30,33,36] (let i = 0; i<=36; i+3);
+
+
+
+//RED/BLACK/EVEN/ODD/LOW NUM/HIGH NUM PAYS EVEN ((1 x BET) + Original Bet = Winnings)
+
+// function () {
+//     if (winConditionCoinFlip) {
+//         return ((`Total Winnings: ${userBet} + ${userBet}`))
+//         }else {
+//             return ((`Round Lost. ${userWallet} - ${userBet}`))
+//         }
+// }
+
+//Red or Black .....let wheelColor = [red, black]
+//Even or Odd .....let wheelEvenOdd = [even, odd]
+//Low Numbers (1-18) .....let wheelNumLow = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+//High Numbers (19-36) .....let wheelNumHigh = [19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36]
